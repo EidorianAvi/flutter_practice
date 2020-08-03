0be5139b52554aad48133a39f41f14bc8bb84656
EidorianAvi <reneavila1993@gmail.com>
2020-08-03 17:06:56 -0600

    Fix quick bug

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,10 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore, _resetQuiz,
+            : Result(
+                _totalScore,
+                _resetQuiz,
+              ),
       ),
     );
   }
